e7f156b862bfc1d3c45846d00c2b7f79292aa4b9
Sebastiano Vierk <sebastiano.vierk@capgemini.com>
2022-01-05 17:12:04 +0100

    chore(setup): add eslint config and necessary packages

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,12 +13,14 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'angular-bootstrap-toast-service'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app.title).toEqual('angular-bootstrap-toast-service');
   });
 
@@ -26,6 +28,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
+
     expect(compiled.querySelector('.content span')?.textContent).toContain(
       'angular-bootstrap-toast-service app is running!'
     );
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
@@ -10,4 +9,5 @@ if (environment.production) {
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
+  // eslint-disable-next-line no-console
   .catch((err) => console.error(err));
