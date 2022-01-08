81d36274b78982db975dfa07fbfdcb52ab30aa4c
Sebastiano Vierk <sebastiano.vierk@capgemini.com>
2022-01-08 15:43:40 +0100

    feat(toasts): add different toast types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EventTypes } from './models/event-types';
 import { ToastService } from './services/toast.service';
 
 @Component({
@@ -9,9 +10,26 @@ import { ToastService } from './services/toast.service';
 export class AppComponent {
   title = 'angular-bootstrap-toast-service';
 
+  EventTypes = EventTypes;
+
   constructor(private toastService: ToastService) {}
 
-  showToast() {
-    this.toastService.showSuccessToast('Hello', 'World');
+  showToast(type: EventTypes) {
+    switch (type) {
+      case EventTypes.Success:
+        this.toastService.showSuccessToast('Success toast title', 'This is a success toast message.');
+        break;
+      case EventTypes.Info:
+        this.toastService.showInfoToast('Info toast title', 'This is an info toast message.');
+        break;
+      case EventTypes.Warning:
+        this.toastService.showWarningToast('Warning toast title', 'This is a warning toast message.');
+        break;
+      case EventTypes.Error:
+        this.toastService.showErrorToast('Error toast title', 'This is an error toast message.');
+        break;
+      default:
+        break;
+    }
   }
 }
