469588a07200053be654e936962d280c0b3fdd8a
Sebastiano Vierk <sebastiano.vierk@capgemini.com>
2022-01-08 13:57:24 +0100

    test(toasts): add unit tests for toast service

--- src/app/services/toast.service.spec.ts
+++ src/app/services/toast.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { EventTypes } from '../models/event-types';
 import { ToastService } from './toast.service';
 
 describe('ToastService', () => {
@@ -9,7 +10,63 @@ describe('ToastService', () => {
     service = TestBed.inject(ToastService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should fire success toast event', (done) => {
+    // given
+    const title = 'some title for success toast';
+    const message = 'some message for success toast';
+
+    service.toastEvents.subscribe((event) => {
+      // then
+      expect(event).toEqual({ type: EventTypes.Success, title, message });
+      done();
+    });
+
+    // when
+    service.showSuccessToast(title, message);
+  });
+
+  it('should fire info toast event', (done) => {
+    // given
+    const title = 'some title for info toast';
+    const message = 'some message for info toast';
+
+    service.toastEvents.subscribe((event) => {
+      // then
+      expect(event).toEqual({ type: EventTypes.Info, title, message });
+      done();
+    });
+
+    // when
+    service.showInfoToast(title, message);
+  });
+
+  it('should fire warning toast event', (done) => {
+    // given
+    const title = 'some title for warning toast';
+    const message = 'some message for warning toast';
+
+    service.toastEvents.subscribe((event) => {
+      // then
+      expect(event).toEqual({ type: EventTypes.Warning, title, message });
+      done();
+    });
+
+    // when
+    service.showWarningToast(title, message);
+  });
+
+  it('should fire error toast event', (done) => {
+    // given
+    const title = 'some title for error toast';
+    const message = 'some message for error toast';
+
+    service.toastEvents.subscribe((event) => {
+      // then
+      expect(event).toEqual({ type: EventTypes.Error, title, message });
+      done();
+    });
+
+    // when
+    service.showErrorToast(title, message);
   });
 });
