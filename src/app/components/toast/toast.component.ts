ee59afc4751091699ccbed7741b305cdc33804aa
Sebastiano Schwarz <s.vierk@hotmail.de>
2025-02-06 09:26:06 +0100

    chore(deps): ng update @angular/core@19 @angular/cli@19

--- src/app/components/toast/toast.component.ts
+++ src/app/components/toast/toast.component.ts
@@ -8,6 +8,7 @@ import { EventTypes } from 'src/app/models/event-types';
   selector: 'app-toast',
   templateUrl: './toast.component.html',
   styleUrls: ['./toast.component.scss'],
+  standalone: false,
 })
 export class ToastComponent implements OnInit {
   @Output() disposeEvent = new EventEmitter();
